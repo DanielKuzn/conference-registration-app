41a44013bef2e3edf3ac5150015bbbeb95692434
DanielKuzn <kuznickidaniel@wp.pl>
2025-06-15 15:43:22 +0200

    Added documentation to ticket class

--- src/ticket.py
+++ src/ticket.py
@@ -1,14 +1,28 @@
 class Ticket:
     """
-    Represents a ticket type with a price and access level.
+    Represents a conference ticket.
+
+    Attributes:
+        ticket_type (str): Type of the ticket (e.g., 'Standard', 'VIP').
+        price (float): Price of the ticket.
     """
 
     def __init__(self, ticket_type: str, price: float, access_level: str):
+        """
+        Initializes a Ticket instance.
+
+        Args:
+            ticket_type (str): Type of the ticket.
+            price (float): Price of the ticket.
+        """
         self.ticket_type = ticket_type
         self.price = price
         self.access_level = access_level
 
     def get_price(self) -> float:
+        """
+        Return the price of the ticket.
+        """
         return self.price
 
     def __str__(self):
